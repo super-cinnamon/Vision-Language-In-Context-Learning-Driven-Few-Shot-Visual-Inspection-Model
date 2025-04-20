02c5c32cb1fb2e82dfb92110fe5d76d63a6ed33d
Yasmine TOLBA <yasmine001tlb@gmail.com>
2025-04-20 13:43:34 +0100

    bring back flash attention

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -2,12 +2,14 @@
 # Adopted from tatsu-lab@stanford_alpaca. Below is the original copyright:
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 
-# Need to call this before importing transformers.
-#from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
-
-#replace_llama_attn_with_flash_attn()
 import sys
 sys.path.append("/kaggle/working/Vision-Language-In-Context-Learning-Driven-Few-Shot-Visual-Inspection-Model")
+
+# Need to call this before importing transformers.
+from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+
+replace_llama_attn_with_flash_attn()
+
 
 from llava.train.train import train
 
